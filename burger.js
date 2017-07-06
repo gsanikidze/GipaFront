24556956c4ffbd1bd21d23f3bf78861ac71b3e47
sano <guram.sanikidze02@geolab.edu.ge>
2017-07-06 18:30:37 +0400

    full responsive with navigation and search

---
+++ burger.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+function openSearch() {
+  document.getElementById("mySearch").style.display = "block";
+    document.getElementById("mySearch").style.width = "100%";
+}
+function closeSearch() {
+    document.getElementById("mySearch").style.width = "0%";
+}
